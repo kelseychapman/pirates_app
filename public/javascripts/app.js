4994c610b5317e1640ec4adea60e7450a82420e8
Kelsey Chapman <kelseyc.chapman@gmail.com>
2016-11-02 11:50:16 -0600

    wire up angular routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,8 @@
-var app = angular.module('pirates', [])
-
-app.controller('piratesController', function($scope) {
- $scope.view = {};
- $scope.view.total = 4;
+var app = angular.module('pirates', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '../views/pirates.html',
+        controller: 'PiratesController'
+      })
 });
